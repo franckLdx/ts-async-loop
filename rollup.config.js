acb0f57de0e05571733e4c241bc107492ef81253
Utilisateur <franck.ledoux.pro@gmail.com>
2023-10-12 19:18:20 +0200

    full rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
-import pkg from './package.json' assert { type: "json" };
 import { dts } from "rollup-plugin-dts";
+import terser from '@rollup/plugin-terser';
 
+import pkg from './package.json' assert { type: "json" };
 
 const input = 'src/asyncLoop.ts'
 
@@ -13,12 +14,14 @@ export default [
     output: {
       name: pkg.name,
       file: pkg.browser,
-      format: 'umd'
+      format: 'umd',
+      plugins: [terser()]
     },
     plugins: [
       resolve(),
       commonjs(),
-      typescript()]
+      typescript()
+    ]
   },
   {
     input,
@@ -27,13 +30,13 @@ export default [
       typescript()
     ],
     output: [
-      { file: pkg.exports.require, format: 'cjs' },
-      { file: pkg.exports.import, format: 'es' }
-    ]
+      { file: pkg.exports.require, format: 'cjs', plugins: [terser()] },
+      { file: pkg.exports.import, format: 'es', plugins: [terser()] }
+    ],
   },
   {
-    input: "./src/asyncLoop.ts",
+    input,
     output: [{ file: pkg.types, format: "es" }],
     plugins: [dts()],
-  },
+  }
 ];
